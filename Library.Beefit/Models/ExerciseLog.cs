9a7c10a79356ee96c2e9c50b8e9de65f371e2320
JGlynn101 <joshuaglynn002@yahoo.com>
2025-02-05 17:26:37 -0500

    Implemented Issue #7/ Implemented ExerciseLog Model

---
+++ Library.Beefit/Models/ExerciseLog.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library.Beefit.Models
+{
+    public class ExerciseLog
+    {
+        public int Id { get; set; }
+        public int WorkoutId { get; set; }
+        public Workout? Workout { get; set; }
+        public string? ExerciseName { get; set; }
+        public int Sets { get; set; }
+        public int Reps { get; set; }
+        public double Weight { get; set; }
+    }
+}
