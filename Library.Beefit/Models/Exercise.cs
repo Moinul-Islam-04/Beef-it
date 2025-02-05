7a0f2ae0695c06c7ca138aa39137930bc8069448
JGlynn101 <joshuaglynn002@yahoo.com>
2025-02-05 17:23:52 -0500

    Implemted Issue #5. Implemented Exercise model

---
+++ Library.Beefit/Models/Exercise.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library.Beefit.Models
+{
+    public class Exercise
+    {
+        public string? Name { get; set; }
+        public string? Type { get; set; }
+        public string? Muscle { get; set; }
+        public string? Equipment { get; set; }
+        public string? Difficulty { get; set; }
+        public string? Instructions { get; set; }
+    }
+}
