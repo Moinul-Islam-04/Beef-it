507aba717682ac2b5367fb664e38e12076694142
JGlynn101 <joshuaglynn002@yahoo.com>
2025-02-05 17:25:28 -0500

    Implemented Issue #6. Implemented Workout model

---
+++ Library.Beefit/Models/Workout.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library.Beefit.Models
+{
+    public class Workout
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public User? User { get; set; }
+
+        public string? Name { get; set; }
+        public DateTime Date { get; set; }  //DateTime is a built in C# struct. Constructs with current time
+        public ICollection<ExerciseLog>? ExerciseLogs { get; set; }
+    }
+}
