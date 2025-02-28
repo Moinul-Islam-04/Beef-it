c3a14ef3bc23623bdd2ac7da49b301cec3332ef7
Moinul-Islam <grimreaper5409@gmail.com>
2025-02-28 13:59:09 -0500

    Mo - manually added nutrition pages

--- Beef--it/LandingPage/LandingPage.xaml.cs
+++ Beef--it/LandingPage/LandingPage.xaml.cs
@@ -16,7 +16,7 @@ namespace Beef__it
         }
         private async void NutritionButton_Clicked(object sender, EventArgs e)
         {
-            await DisplayAlert("Nutrition", "Nutrition button has been clicked", "OK");
+            await Navigation.PushAsync(NutritionPage());
         }
         private async void ProgressPicButton_Clicked(object sender, EventArgs e) {
             await Navigation.PushAsync(new ProgressPicPage());
